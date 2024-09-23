739eca0754018202360aa103a67f8a820a586e46
Muammar <kigedensleman@hotmail.com>
2024-09-23 22:18:49 +0700

    Add files via upload

---
+++ db_amart.sql
@@ -0,0 +1,298 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 06, 2022 at 07:22 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_amart`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_jual`
+--
+
+CREATE TABLE `tbl_jual` (
+  `id_jual` int(11) NOT NULL,
+  `no_faktur` varchar(15) DEFAULT NULL,
+  `tgl_jual` date DEFAULT NULL,
+  `jam` time DEFAULT NULL,
+  `grand_total` int(11) DEFAULT NULL,
+  `dibayar` int(11) DEFAULT NULL,
+  `diskon` int(11) DEFAULT NULL,
+  `kembalian` int(11) NOT NULL,
+  `id_user` int(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_jual`
+--
+
+INSERT INTO `tbl_jual` (`id_jual`, `no_faktur`, `tgl_jual`, `jam`, `grand_total`, `dibayar`, `diskon`, `kembalian`, `id_user`) VALUES
+(14, '202209010001', '2022-09-01', '05:23:26', 77000, 80000, NULL, 3000, 1),
+(15, '202209110001', '2022-09-11', '05:25:55', 128000, 130000, NULL, 2000, 1),
+(16, '202210060001', '2022-10-06', '05:27:01', 56000, 60000, NULL, 4000, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kategori`
+--
+
+CREATE TABLE `tbl_kategori` (
+  `id_kategori` int(2) NOT NULL,
+  `nama_kategori` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_kategori`
+--
+
+INSERT INTO `tbl_kategori` (`id_kategori`, `nama_kategori`) VALUES
+(1, 'Rokok'),
+(2, 'Makanan dan Minuman');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_produk`
+--
+
+CREATE TABLE `tbl_produk` (
+  `id_produk` int(11) NOT NULL,
+  `kode_produk` varchar(25) DEFAULT NULL,
+  `nama_produk` varchar(150) DEFAULT NULL,
+  `id_kategori` int(2) DEFAULT NULL,
+  `id_satuan` int(2) DEFAULT NULL,
+  `harga_beli` int(11) DEFAULT NULL,
+  `harga_jual` int(11) DEFAULT NULL,
+  `stok` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_produk`
+--
+
+INSERT INTO `tbl_produk` (`id_produk`, `kode_produk`, `nama_produk`, `id_kategori`, `id_satuan`, `harga_beli`, `harga_jual`, `stok`) VALUES
+(1, '8998989300261', 'GG. Surya 12', 1, 1, 20050, 22000, 11),
+(2, '8998989110167', 'GG. Surya 16', 1, 1, 25550, 28000, 9),
+(3, '8998989100120', 'GG. Filter', 1, 1, 20150, 22000, 9),
+(4, '8999909028999', 'Dji Sam Soe Refill', 1, 1, 18350, 21000, 9),
+(5, '8999909028234', 'Dji Sam Soe Kretek 12', 1, 1, 17250, 20000, 8),
+(6, '8999909010567', 'Dji Sam Soe Kretek 16', 1, 1, 25600, 28000, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_rinci_jual`
+--
+
+CREATE TABLE `tbl_rinci_jual` (
+  `id_rinci` int(11) NOT NULL,
+  `no_faktur` varchar(15) DEFAULT NULL,
+  `kode_produk` varchar(25) DEFAULT NULL,
+  `modal` int(11) DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `total_harga` int(11) DEFAULT NULL,
+  `profit` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_rinci_jual`
+--
+
+INSERT INTO `tbl_rinci_jual` (`id_rinci`, `no_faktur`, `kode_produk`, `modal`, `harga`, `qty`, `total_harga`, `profit`) VALUES
+(20, '202209010001', '8999909010567', 25600, 28000, 1, 28000, 2400),
+(21, '202209010001', '8998989110167', 25550, 28000, 1, 28000, 2450),
+(22, '202209010001', '8999909028999', 18350, 21000, 1, 21000, 2650),
+(23, '202209110001', '8998989100120', 20150, 22000, 1, 22000, 1850),
+(24, '202209110001', '8998989300261', 20050, 22000, 3, 66000, 5850),
+(25, '202209110001', '8999909028234', 17250, 20000, 2, 40000, 5500),
+(26, '202210060001', '8999909010567', 25600, 28000, 2, 56000, 4800);
+
+--
+-- Triggers `tbl_rinci_jual`
+--
+DELIMITER $$
+CREATE TRIGGER `t_rinci_jual` AFTER INSERT ON `tbl_rinci_jual` FOR EACH ROW BEGIN
+UPDATE tbl_produk SET stok = stok - NEW.qty
+WHERE kode_produk = NEW.kode_produk;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_satuan`
+--
+
+CREATE TABLE `tbl_satuan` (
+  `id_satuan` int(2) NOT NULL,
+  `nama_satuan` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_satuan`
+--
+
+INSERT INTO `tbl_satuan` (`id_satuan`, `nama_satuan`) VALUES
+(1, 'Bungkus'),
+(2, 'PCS');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_setting`
+--
+
+CREATE TABLE `tbl_setting` (
+  `id` int(1) NOT NULL,
+  `nama_toko` varchar(25) DEFAULT NULL,
+  `slogan` varchar(50) DEFAULT NULL,
+  `alamat` varchar(200) DEFAULT NULL,
+  `no_telepon` varchar(18) DEFAULT NULL,
+  `deskripsi` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_setting`
+--
+
+INSERT INTO `tbl_setting` (`id`, `nama_toko`, `slogan`, `alamat`, `no_telepon`, `deskripsi`) VALUES
+(1, 'A-Mart', 'Andasuka Harga Suka-suka', 'Blok Tlaga Desa Sleman Lor ', '089660202099', 'Menyediakan Berbagai macam Barang Kebutuhan Rumah Tangga');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id_user` int(2) NOT NULL,
+  `nama_user` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `level` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id_user`, `nama_user`, `email`, `password`, `level`) VALUES
+(1, 'ammar', 'admin@admin.com', 'bb0a882c09a5db39d7fbe6d926886751028cd54d', 1),
+(2, 'Aisyah', 'kasir@kasir.com', 'bb0a882c09a5db39d7fbe6d926886751028cd54d', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_jual`
+--
+ALTER TABLE `tbl_jual`
+  ADD PRIMARY KEY (`id_jual`);
+
+--
+-- Indexes for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `tbl_produk`
+--
+ALTER TABLE `tbl_produk`
+  ADD PRIMARY KEY (`id_produk`);
+
+--
+-- Indexes for table `tbl_rinci_jual`
+--
+ALTER TABLE `tbl_rinci_jual`
+  ADD PRIMARY KEY (`id_rinci`);
+
+--
+-- Indexes for table `tbl_satuan`
+--
+ALTER TABLE `tbl_satuan`
+  ADD PRIMARY KEY (`id_satuan`);
+
+--
+-- Indexes for table `tbl_setting`
+--
+ALTER TABLE `tbl_setting`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_jual`
+--
+ALTER TABLE `tbl_jual`
+  MODIFY `id_jual` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  MODIFY `id_kategori` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_produk`
+--
+ALTER TABLE `tbl_produk`
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tbl_rinci_jual`
+--
+ALTER TABLE `tbl_rinci_jual`
+  MODIFY `id_rinci` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `tbl_satuan`
+--
+ALTER TABLE `tbl_satuan`
+  MODIFY `id_satuan` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_setting`
+--
+ALTER TABLE `tbl_setting`
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id_user` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
